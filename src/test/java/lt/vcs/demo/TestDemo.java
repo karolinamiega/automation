272d8558f2d1366e0befcbb3f0b929165719052d
karolinamiega <nugaraitekarolina@gmail.com>
2024-11-27 20:04:33 +0200

    Add testNg demo examples

--- src/test/java/lt/vcs/demo/TestDemo.java
+++ src/test/java/lt/vcs/demo/TestDemo.java
@@ -1,26 +1,91 @@
 package lt.vcs.demo;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class TestDemo {
 
     @Test
-    public void test01(){
+    public void testIsFourEven() {
+        int number = 4;
+        int expectedResult = 0;
+        int actualResult;
 
+        actualResult = number % 2;
+        Assert.assertEquals(actualResult, expectedResult);
     }
 
     @Test
-    public void test02(){
+    public void testNegativeFourIsNotEven() {
+        int number = 4;
+        int expectedResult = 1;
+        int actualResult;
 
+        actualResult = number % 2;
+        Assert.assertNotEquals(actualResult, expectedResult);
+//        Assert.assertTrue(actualResult != expectedResult);
+//        Assert.assertFalse(actualResult == expectedResult);
     }
 
     @Test
-    public void test03(){
+    public void testTimeNowIs19() {
+        int expectedResult = 19;
+        int actualResult;
+
+        actualResult = LocalTime.now().getHour();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    //    Testas “žalias” jeigu 995 dalijasi iš 3 (be liekanos)
+    @Test
+    public void test995DividesBy3() {
+        int num = 995;
+        int actualResult = num % 3;
+        int expectedResult = 0;
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+
+    //    Testas “žalias” jeigu šiandien trečiadienis
+    @Test
+    public void testIfTodayIsWednesday() {
+        String expectedResult = "WEDNESDAY";
+        String actualResult = String.valueOf(LocalDate.now().getDayOfWeek());
+
+        Assert.assertEquals(actualResult, expectedResult);
 
     }
 
+    //    Testas “žalias” po to, kai “palaukia” 5 sekundes
     @Test
-    public void test(){
+    public void testWaitsFiveSeconds() throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        Thread.sleep(5000);
+        long endTime = System.currentTimeMillis();
+        long actualResult = endTime - startTime;
+        long expectedResult = 5000;
+        long tolerance = 100;
+
+        Assert.assertTrue((actualResult - expectedResult) <= tolerance);
+
+    }
+//    Bonus: Testas “žalias” jei nuo 1 iki 10 yra 4 lyginiai skaičia
+
+    @Test
+    public void testCountOfEvenNumbersBetween1To10() {
+        int expectedResult = 4;
+        int actualResult = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                actualResult++;
+            }
+        }
+        Assert.assertEquals(actualResult, expectedResult);
 
     }
 }
