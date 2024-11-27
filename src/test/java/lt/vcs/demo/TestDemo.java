2dc46d99c3919c67cc207dfa4a4b3651a1d6f783
karolinamiega <nugaraitekarolina@gmail.com>
2024-11-27 18:36:45 +0200

    Initial commit

---
+++ src/test/java/lt/vcs/demo/TestDemo.java
@@ -0,0 +1,26 @@
+package lt.vcs.demo;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+
+    @Test
+    public void test01(){
+
+    }
+
+    @Test
+    public void test02(){
+
+    }
+
+    @Test
+    public void test03(){
+
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
